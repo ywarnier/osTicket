1faf2cc787bf66548b7a6e4a5ef9a498a4517615
Yannick Warnier <ywarnier@beeznest.org>
2024-08-02 15:48:26 +0200

    Language: Add fr_BE language pack - refs BT#21291

---
+++ include/i18n/fr_BE/MANIFEST.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Build-Date' => 'Thu, 13 Jul 23 19:42:49 +0000',
+  'Phrases-Version' => '1.18',
+  'Build-Version' => 'v1.18',
+  'Build-Major-Version' => '1.18',
+  'Language' => 'fr_BE',
+  'Id' => 'lang:fr',
+  'Last-Revision' => '2023-07-11 20:58',
+  'Version' => 168910,
+);
