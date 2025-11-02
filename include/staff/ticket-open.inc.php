d47a25253d5780f0b20b441698de88577cc0f902
Yannick Warnier <ywarnier@beeznest.org>
2025-11-03 00:39:10 +0100

    Ticket add: Change submit button to "Save" and hide reset button - refs BT#23059

--- include/staff/ticket-open.inc.php
+++ include/staff/ticket-open.inc.php
@@ -475,8 +475,8 @@ print $response_form->getField('attachments')->render();
     </tbody>
 </table>
 <p style="text-align:center;">
-    <input type="submit" name="submit" value="<?php echo _P('action-button', 'Open');?>">
-    <input type="reset"  name="reset"  value="<?php echo __('Reset');?>">
+    <input type="submit" name="submit" value="<?php echo _P('action-button', 'Save');?>">
+    <!--input type="reset"  name="reset"  value="<?php echo __('Reset');?>"-->
     <input type="button" name="cancel" value="<?php echo __('Cancel');?>" onclick="javascript:
         $(this.form).find('textarea.richtext')
           .redactor('plugin.draft.deleteDraft');
